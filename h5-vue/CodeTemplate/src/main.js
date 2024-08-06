ff26b1eebfedcb04aa923956769a91acb31d41b4
Dysonliu <dysonliu@rektec.com.cn>
2024-08-06 11:20:08 +0800

    element-plus语言文件编译报错

--- h5-vue/CodeTemplate/src/main.js
+++ h5-vue/CodeTemplate/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
+import locale from 'element-plus/dist/locale/zh-cn.mjs'
 import './assets/main.css'
 
 const app = createApp(App)
